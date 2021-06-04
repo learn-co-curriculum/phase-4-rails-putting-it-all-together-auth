6f8eab77f592612bfad2ef53389c7f2e1e5554b4
ihollander <ianholla@gmail.com>
2021-06-04 07:02:35 -0400

    Added React app

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { Button } from "../styles";
 
 function NavBar({ user, setUser }) {
   function handleLogoutClick() {
@@ -11,22 +13,47 @@ function NavBar({ user, setUser }) {
   }
 
   return (
-    <header>
-      <div>
-        <Link to="/">Home</Link>
-      </div>
-      <div>
-        {user ? (
-          <button onClick={handleLogoutClick}>Logout</button>
-        ) : (
-          <>
-            <Link to="/signup">Signup</Link>
-            <Link to="/login">Login</Link>
-          </>
-        )}
-      </div>
-    </header>
+    <Wrapper>
+      <Logo>
+        <Link to="/">Reciplease</Link>
+      </Logo>
+      <Nav>
+        <Button as={Link} to="/new">
+          New Recipe
+        </Button>
+        <Button variant="outline" onClick={handleLogoutClick}>
+          Logout
+        </Button>
+      </Nav>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.header`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 8px;
+`;
+
+const Logo = styled.h1`
+  font-family: "Permanent Marker", cursive;
+  font-size: 3rem;
+  color: deeppink;
+  margin: 0;
+  line-height: 1;
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  gap: 4px;
+  position: absolute;
+  right: 8px;
+`;
 
 export default NavBar;
--- client/src/components/SignUpForm.js
+++ client/src/components/SignUpForm.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
+import { Button, Error, Input, FormField, Label, Textarea } from "../styles";
 
-function SignUpForm({ setUser }) {
+function SignUpForm({ onLogin }) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [passwordConfirmation, setPasswordConfirmation] = useState("");
   const [imageUrl, setImageUrl] = useState("");
   const [bio, setBio] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   function handleSubmit(e) {
     e.preventDefault();
+    setErrors([]);
+    setIsLoading(true);
     fetch("/signup", {
       method: "POST",
       headers: {
@@ -18,59 +23,78 @@ function SignUpForm({ setUser }) {
         username,
         password,
         password_confirmation: passwordConfirmation,
+        image_url: imageUrl,
+        bio,
       }),
     }).then((r) => {
+      setIsLoading(false);
       if (r.ok) {
-        r.json().then((user) => setUser(user));
+        r.json().then((user) => onLogin(user));
+      } else {
+        r.json().then((err) => setErrors(err.errors));
       }
     });
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <h1>Sign Up</h1>
-        <label htmlFor="username">Username</label>
-        <input
+    <form onSubmit={handleSubmit}>
+      <FormField>
+        <Label htmlFor="username">Username</Label>
+        <Input
           type="text"
           id="username"
           autoComplete="off"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
         />
-        <label htmlFor="password">Password</label>
-        <input
+      </FormField>
+      <FormField>
+        <Label htmlFor="password">Password</Label>
+        <Input
           type="password"
           id="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           autoComplete="current-password"
         />
-        <label htmlFor="password">Password Confirmation</label>
-        <input
+      </FormField>
+      <FormField>
+        <Label htmlFor="password">Password Confirmation</Label>
+        <Input
           type="password"
           id="password_confirmation"
           value={passwordConfirmation}
           onChange={(e) => setPasswordConfirmation(e.target.value)}
           autoComplete="current-password"
         />
-        <label htmlFor="imageUrl">Profile Image</label>
-        <input
+      </FormField>
+      <FormField>
+        <Label htmlFor="imageUrl">Profile Image</Label>
+        <Input
           type="text"
           id="imageUrl"
           value={imageUrl}
           onChange={(e) => setImageUrl(e.target.value)}
         />
-        <label htmlFor="bio">Bio</label>
-        <textarea
+      </FormField>
+      <FormField>
+        <Label htmlFor="bio">Bio</Label>
+        <Textarea
           rows="3"
           id="bio"
           value={bio}
           onChange={(e) => setBio(e.target.value)}
         />
-        <button type="submit">Sign Up</button>
-      </form>
-    </div>
+      </FormField>
+      <FormField>
+        <Button type="submit">{isLoading ? "Loading..." : "Sign Up"}</Button>
+      </FormField>
+      <FormField>
+        {errors.map((err) => (
+          <Error key={err}>{err}</Error>
+        ))}
+      </FormField>
+    </form>
   );
 }
 
