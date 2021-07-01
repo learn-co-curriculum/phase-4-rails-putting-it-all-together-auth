bda071f223d33aad2c2b8f15cc4f7ec2ffa6d80e
Liz Burton <liz.burton147@gmail.com>
2021-07-01 09:59:33 -0400

    Fix typo

--- spec/requests/recipes_spec.rb
+++ spec/requests/recipes_spec.rb
@@ -68,7 +68,7 @@ RSpec.describe "Recipes", type: :request do
       end
 
       it "returns a 401 (Unauthorized) HTTP status code" do
-        post "/recipes"
+        get "/recipes"
 
         expect(response).to have_http_status(:unauthorized)
       end
