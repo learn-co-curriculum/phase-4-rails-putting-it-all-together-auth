ca9d08ef1862e2985971a68b3a281efdcb7819c6
Bianca <biancancanal@gmail.com>
2021-11-23 14:14:16 -0600

    commit tree e0cbc7b463103c45b943e9907158216634275e0f

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :recipes, only: [:index, :create]
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20211123000145_create_recipes.rb
+++ db/migrate/20211123000145_create_recipes.rb
@@ -1,6 +1,7 @@
 class CreateRecipes < ActiveRecord::Migration[6.1]
   def change
     create_table :recipes do |t|
+      t.belongs_to :user, null: false, foreign_key: true
       t.string :title
       t.text :instructions
       t.integer :minutes_to_complete
