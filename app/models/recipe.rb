31f5fdf5499ccc12ae68b7407185d549bf2b287e
Bianca <biancancanal@gmail.com>
2021-11-22 18:06:38 -0600

    Complete Models and Migrations

---
+++ app/models/recipe.rb
@@ -0,0 +1,5 @@
+class Recipe < ApplicationRecord
+    belongs_to :user 
+    validates :title, presence: true 
+    validates :instructions, presence: true, length: { minimum: 50 }
+end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,19 +1,5 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
-  it "can be created with valid data" do
-    user = User.create(username: "test_user", password: "sup3r-secret", image_url: "https://via.placeholder.com/150", bio: "bio")
-    recipe = Recipe.new(
-      user_id: user.id,
-      title: "Rice", 
-      instructions: "Measure 1 cup of rice in bowl of rice cooker. Wash rice. Fill with water to level indicated by manufacturer. Put bowl in rice cooker. Press 'Cook'. Enjoy!",
-      minutes_to_complete: 10
-    )
-    expect(recipe).to be_valid
-  end
-  
-  describe "validations" do
-    it { is_expected.to validate_presence_of :title }
-    it { is_expected.to validate_length_of(:instructions).is_at_least(50) }
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
